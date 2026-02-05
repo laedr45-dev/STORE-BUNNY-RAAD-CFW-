f525df9d5ce085607228303292d84562338d0a4a
laedr45-dev <laedr45@gmail.com>
2026-02-05 09:27:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function addToCart(item) {
   document.getElementById("cartCount").textContent = cart.length;
   alert("تمت إضافة " + item + " إلى السلة ✅");
 }
+function maintenance() {
+  const msg = event.target.nextElementSibling;
+  msg.classList.remove("hidden");
+}
